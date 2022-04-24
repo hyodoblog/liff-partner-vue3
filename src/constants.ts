ec40b101313d9622555a3413544d0442919695fe
hyodoblog <yusei.hyodo@youlib.co.jp>
2022-04-25 02:09:49 +0900

    liff init

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const IS_PROD = import.meta.env.PROD
+export const LIFF_ID = import.meta.env.VITE_APP_LIFF_ID
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import { IS_PROD, LIFF_ID } from '~/constants'
+import Loading from '~/components/Loading.vue'
+import HelloWorld from '~/components/HelloWorld.vue'
+import liff from '@line/liff'
+
+let isLoggedIn = false
+let userId: string | undefined
+if (IS_PROD) {
+  liff.init({ liffId: LIFF_ID! })
+  userId = liff.getContext()?.userId
+  isLoggedIn = liff.isLoggedIn()
+}
+</script>
+
 <template>
-  <div>aa</div>
+  <HelloWorld v-if="isLoggedIn" :user-id="`${userId}`" />
+  <Loading v-else />
 </template>
