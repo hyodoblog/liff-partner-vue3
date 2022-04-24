0ca0eac641b492b266c3ff01addc0c5d619de7dc
hyodoblog <yusei.hyodo@youlib.co.jp>
2022-04-25 02:31:51 +0900

    liff initの調整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,25 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import liff from '@line/liff'
 import { IS_PROD, LIFF_ID } from '~/constants'
 import Loading from '~/components/Loading.vue'
 import HelloWorld from '~/components/HelloWorld.vue'
-import Error from '~/components/Error.vue';
 
-let isError = false
-let err: any
 let isLoggedIn = false
 let userId: string | null
 
-onMounted(async () => {
-  try {
-    if (IS_PROD) {
-      await liff.init({ liffId: LIFF_ID })
-      userId = liff.getContext()?.userId || null
-      isLoggedIn = liff.isLoggedIn()
-    }
-  } catch (err) {
-    err = err
-    isError = true
-    isLoggedIn = false
-    userId = null
-  }
-})
+if (IS_PROD) {
+  liff.init({ liffId: LIFF_ID })
+  liff.ready.then(() => {
+    userId = liff.getContext()?.userId || null
+    isLoggedIn = liff.isLoggedIn()
+  })
+} else {
+  isLoggedIn = true
+  userId = 'test'
+}
 </script>
 
 <template>
-  <Error v-if="isError" :err="err" />
-  <HelloWorld v-else-if="isLoggedIn" :user-id="`${userId}`" />
+  <HelloWorld v-if="isLoggedIn" :user-id="`${userId}`" />
   <Loading v-else />
-
-  {{ liff }}
 </template>
